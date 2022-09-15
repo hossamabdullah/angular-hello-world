69c24075ce81a785164f9136aaa99cb4a191b1d3
hossam_abdullah <hossam_abdullah@siliconexpert.com>
2022-09-15 14:47:43 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { EventsComponent } from './events/events.component';
+import { DataNotFoundComponent } from './data-not-found/data-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EventsComponent,
+    DataNotFoundComponent
   ],
   imports: [
     BrowserModule
